a3ea24770b0c9b8f98a585ad6f7985abca674fd3
Mark Notton <mark@yellostudio.co.uk>
2019-05-16 11:56:23 +0100

    Fixed - Notifications that have manually be pushed will disappear correctly. It was  only remove the last item, which could be wrong if there are multiple noticed. - Videos input filed would render out an ugly json encoded string. And each save would mutate it further, potentially bloating the input to max out the character count allowed for most Databases. It will now show the santised video URL instead.

--- src/assets/scripts/notifications.js
+++ src/assets/scripts/notifications.js
@@ -5,17 +5,21 @@
 // Success Notice --------------------------------------------------------------
 
 function setNotice(message) {
-  var notice = '<div class="notification notice">'+message+'</div>';
-  $('#notifications').append(notice);
-  setTimeout(function(){ $('.notification.notice:last').fadeOut(500) }, 3000);
+  let notice = document.createElement('div')
+  notice.className = 'notification notice'
+  notice.innerHTML = message
+  document.querySelector('#notifications').appendChild(notice)
+  setTimeout(function(){ $(notice).fadeOut(500) }, 3000);
 }
 
 // Error Notice ----------------------------------------------------------------
 
 function setError(message) {
-  var notice = '<div class="notification error">'+message+'</div>';
-  $('#notifications').append(notice);
-  setTimeout(function(){ $('.notification.error:last').fadeOut(500) }, 3000);
+  let notice = document.createElement('div')
+  notice.className = 'notification error'
+  notice.innerHTML = message
+  document.querySelector('#notifications').appendChild(notice)
+  setTimeout(function(){ $(notice).fadeOut(500) }, 3000);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
